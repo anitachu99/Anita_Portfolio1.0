94a292223350e44f38495637c0af41ec05d35c49
Anita Chu <c8anita@gmail.com>
2024-03-05 15:00:58 -0500

    Completed about me page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,44 @@ module.exports = {
     extend: {
       fontFamily: {
         jost: ["Jost"],
+        jostReg: ["Jost, regular"],
       },
       backgroundImage: {
         'skyImage': "url(/static/images/white-clouds-blue-background.jpg)",
       },
-      animation: {
-        'bounce-slow': 'bounce 1.5s linear infinite',
-      },
       colors: {
-        'greyColor': '#acb4c4'
+        'greyColor': '#acb4c4',
+      },
+      backgroundColor: {
+        'skyBlue': '#429FD1',
+      },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }  
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }  
+        }
+      },
+      animation: {
+        typing: "typing 2s steps(20) alternate, blink .7s",
+        'bounce-slow': 'bounce 1.5s linear infinite',
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
